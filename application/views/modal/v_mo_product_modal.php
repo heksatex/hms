9249856c038a4b54a82f8e423b304a0b8e6be3a4
Galih Gustika <galihdrm@gmail.com>
2022-03-09 10:42:53 +0700

    hapus info lebar di modal  view data product MO

--- application/views/modal/v_mo_product_modal.php
+++ application/views/modal/v_mo_product_modal.php
@@ -18,12 +18,6 @@
 	<div class="form-group">
 		<div class="col-md-6">
 			<div class="col-md-12 col-xs-12">
-                <div class="col-xs-4"><label>Lebar</label></div>
-                <div class="col-xs-8">
-                  	<input type="text" name="lebar" id="lebar" class="form-control input-sm"  value="<?php echo htmlentities($prod['lebar'])?>" readonly="readonly"/>
-                </div>  
-             </div>
-			<div class="col-md-12 col-xs-12">
 	            <div class="col-xs-4"><label>Create Date</label></div>
 	            <div class="col-xs-8">
 	                <input type="text" name="create" id="create" class="form-control input-sm"  value="<?php echo htmlentities($prod['create_date'])?>" readonly="readonly"/>
