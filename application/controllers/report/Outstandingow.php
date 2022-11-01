1b4613cecbb211ccef5b6fa934e6b031db62a838
galihguspa <Galihdrm@gmail.com>
2022-11-01 10:20:19 +0700

    new menu outstanding OW

---
+++ application/controllers/report/Outstandingow.php
@@ -0,0 +1,130 @@
+<?php defined('BASEPATH') OR EXIT ('No Direct Script Acces Allowed');
+
+/**
+ * 
+ */
+class Outstandingow extends MY_Controller
+{
+
+    public function __construct()
+	{
+		parent:: __construct();
+		$this->is_loggedin();//cek apakah user sudah login
+		$this->load->model('_module');
+		$this->load->model('m_outstandingOW');
+	}
+
+    public function index()
+	{
+		$id_dept        = 'OTSOW';
+        $data['id_dept']= $id_dept;
+		$data['mst_sales_group'] = $this->_module->get_list_sales_group();
+		$this->load->view('report/v_outstanding_ow', $data);
+	}
+
+    public function get_data()
+	{   
+
+        $list = $this->m_outstandingOW->get_datatables();
+        $data = array();
+        $no = $_POST['start'];
+        foreach ($list as $field) {
+        
+            if($field->status_scl == 't'){
+                $status_scl = 'Aktif';
+            }else if($field->status_scl == 'ng'){
+                $status_scl = 'Not Good';
+            }else{
+                $status_scl = 'Tidak Aktif';
+            }
+
+            $no++;
+            $row = array();
+            $row[] = $no;
+            $row[] = $field->sales_order;
+            $row[] = $field->nama_sales_group;
+            $row[] = $field->ow;
+            $row[] = $field->tanggal_ow;
+            $row[] = $status_scl;
+            $row[] = $field->nama_produk;
+            $row[] = $field->nama_warna;
+            $row[] = number_format($field->qty,2).' '.$field->uom;
+         
+            $data[] = $row;
+            
+        }
+ 
+        $output = array(
+            "draw" => $_POST['draw'],
+            "recordsTotal" => $this->m_outstandingOW->count_all(),
+            "recordsFiltered" => $this->m_outstandingOW->count_filtered(),
+            "data" => $data,
+        );
+        //output dalam format JSON
+        echo json_encode($output);
+	}
+
+    public function export_excel()
+    {   
+
+		$sc             = $this->input->post('sc');
+		$sales_group    = $this->input->post('sales_group');
+		$ow             = $this->input->post('ow');
+		$produk         = $this->input->post('produk');
+		$warna          = $this->input->post('warna');
+
+        $this->load->library('excel');
+        $object = new PHPExcel();
+    	$object->setActiveSheetIndex(0);
+
+        // SET JUDUL
+ 		$object->getActiveSheet()->SetCellValue('A1', 'Laporan Outstanding OW');
+ 		$object->getActiveSheet()->getStyle('A1')->getAlignment()->setIndent(1);
+		$object->getActiveSheet()->mergeCells('A1:L1');
+
+        //bold huruf
+		$object->getActiveSheet()->getStyle("A1:T3")->getFont()->setBold(true);
+
+        // header table
+    	$table_head_columns  = array('No', 'No.SC', 'Kode MKT', 'No.OW', 'Tgl OW', 'Status OW', 'Nama Produk', 'Warna', 'Qty', 'Uom');
+        $column = 0;
+        foreach ($table_head_columns as $field) {
+            $object->getActiveSheet()->setCellValueByColumnAndRow($column, 3, $field);  
+    		$column++;
+    	}
+
+        $num   = 1;
+        $rowCount = 4;
+        $list = $this->m_outstandingOW->get_list_ow_by_kode($sc,$sales_group,$ow,$produk,$warna);
+        foreach($list as $val){
+
+            if($val->status_scl == 't'){
+                $status_scl = 'Aktif';
+            }else if($val->status_scl == 'ng'){
+                $status_scl = 'Not Good';
+            }else{
+                $status_scl = 'Tidak Aktif';
+            }
+
+            $object->getActiveSheet()->SetCellValue('A'.$rowCount, ($num++));
+			$object->getActiveSheet()->SetCellValue('B'.$rowCount, $val->sales_order);
+			$object->getActiveSheet()->SetCellValue('C'.$rowCount, $val->nama_sales_group);
+			$object->getActiveSheet()->SetCellValue('D'.$rowCount, $val->ow);
+			$object->getActiveSheet()->SetCellValue('E'.$rowCount, $val->tanggal_ow);
+			$object->getActiveSheet()->SetCellValue('F'.$rowCount, $status_scl);
+			$object->getActiveSheet()->SetCellValue('G'.$rowCount, $val->nama_produk);
+			$object->getActiveSheet()->SetCellValue('H'.$rowCount, $val->nama_warna);
+			$object->getActiveSheet()->SetCellValue('I'.$rowCount, $val->qty);
+			$object->getActiveSheet()->SetCellValue('J'.$rowCount, $val->uom);
+	        $rowCount++;
+        }
+
+      
+        $object = PHPExcel_IOFactory::createWriter($object, 'Excel2007');  
+
+        header('Content-Type: application/vnd.ms-excel'); //mime type
+        header('Content-Disposition: attachment;filename="Outstanding OW.xlsx"'); //tell browser what's the file name
+        header('Cache-Control: max-age=0'); //no cache
+        $object->save('php://output');
+    }
+}
---
+++ application/views/report/v_outstanding_ow.php
@@ -0,0 +1,287 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php $this->load->view("admin/_partials/head.php") ?>
+  <style type="text/css">
+    
+    h3{
+      display: block !important;
+      text-align: center !important;
+    }
+
+    .divListviewHead table  {
+      display: block;
+      height: calc( 100vh - 250px );
+      overflow-x: auto;
+    }
+
+  </style>
+</head>
+
+<body class="hold-transition skin-black fixed sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- main -header -->
+  <header class="main-header">
+   <?php $this->load->view("admin/_partials/main-menu.php") ?>
+   <?php $this->load->view("admin/_partials/topbar.php") ?>
+  </header>
+
+  <!-- Menu Side Bar -->
+  <aside class="main-sidebar">
+  <?php $this->load->view("admin/_partials/sidebar.php") ?>
+  </aside>
+
+  <!-- Content Wrapper-->
+  <div class="content-wrapper">
+    <!-- Content Header (Status - Bar) -->
+    <section class="content-header">
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <!--  box content -->
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title"><b>Outstanding OW</b></h3>
+        </div>
+        <div class="box-body">
+
+            <form name="input" class="form-horizontal" role="form" method="POST" id="frm_job_list" action="<?=base_url()?>report/outstandingow/export_excel">
+                <div class="col-md-8">
+                  <div class="form-group">
+                    <div class="col-md-6"> 
+                        <div class="form-group">
+                            <div class="col-md-5">
+                              <label>SC </label>
+                            </div>
+                            <div class="col-md-7">
+                                <input type="text" class="form-control input-sm" name="sc" id="sc" onkeydown=" event_input(event)" >
+                            </div>
+                        </div> 
+                        <div class="form-group">
+                            <div class="col-md-5">
+                              <label>Marketing </label>
+                            </div>
+                            <div class="col-md-7">
+                                <select class="form-control input-sm" name="sales_group" id="sales_group"  onkeydown="event_input(event)" >
+                                    <option value=''>--Pilih Marketing--</option>
+                                    <?php foreach($mst_sales_group as $msg){
+                                            echo '<option value="'.$msg->kode_sales_group.'">'.$msg->nama_sales_group.'</option>';
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-5">
+                              <label>OW </label>
+                            </div>
+                            <div class="col-md-7">
+                                <input type="text" class="form-control input-sm" name="ow" id="ow"  onkeydown="event_input(event)">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <div class="col-md-5">
+                              <label>Produk </label>
+                            </div>
+                            <div class="col-md-7">
+                                <input type="text" class="form-control input-sm" name="produk" id="produk" onkeydown="event_input(event)" >
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-5">
+                              <label>Warna </label>
+                            </div>
+                            <div class="col-md-7">
+                                <input type="text" class="form-control input-sm" name="warna" id="warna"  onkeydown="event_input(event)">
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                    <button type="button" class="btn btn-sm btn-default" name="btn-filter" id="btn-filter" >Proses</button>
+                    <button type="submit" class="btn btn-sm btn-default" name="btn-generate" id="btn-excel" > <i class="fa fa-file-excel-o"></i> Excel</button>
+                </div>
+            </form>
+            <br>
+
+            <!-- table -->
+            <div class="box-body">
+              <div class="col-xs-12 table-responsive">
+                <table id="example1" class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>No.SC</th>
+                      <th>Kode MKT</th>
+                      <th>No.OW</th>
+                      <th>Tgl OW</th>
+                      <th>Status OW</th>
+                      <th>Product</th>
+                      <th>Warna</th>
+                      <th>Qty</th>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+            </div>
+
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+</div>
+<?php $this->load->view("admin/_partials/js.php") ?>
+
+<script type="text/javascript">
+
+    //* Show collapse advanced search
+    $('#advancedSearch').on('shown.bs.collapse', function () {
+        $(".showAdvanced").removeClass("glyphicon-triangle-bottom").addClass("glyphicon-triangle-top");
+    });
+
+    //* Hide collapse advanced search
+    $('#advancedSearch').on('hidden.bs.collapse', function () {
+        $(".showAdvanced").removeClass("glyphicon-triangle-top").addClass("glyphicon-triangle-bottom");
+    });
+
+    var d     = new Date();
+    var month = d.getMonth();
+    var day   = d.getDate();
+    var day_30 = d.getDate()-30;
+    var year  = d.getFullYear();
+
+    // set date tgldari
+    $('#tgldari').datetimepicker({
+        defaultDate : new Date(year, month, day_30),
+        format : 'D-MMMM-YYYY',
+        ignoreReadonly: true,
+        maxDate: new Date
+        
+    });
+
+    // set date tglsampai
+    $('#tglsampai').datetimepicker({
+        defaultDate : new Date(),
+        format : 'D-MMMM-YYYY',
+        ignoreReadonly: true,
+        maxDate: new Date
+    });
+
+    $(document).ready(function() {
+
+        $('#example1').DataTable({ 
+            "dom": "<'row'<'col-sm-4'l><'col-sm-5'i><'col-sm-3'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-5'><'col-sm-7'p>>",
+            "aLengthMenu": [[50, 100, 500, 1000, -1], [50, 100, 500, 1000, "All"]],
+            "iDisplayLength": 50,
+            "paging": true,
+            "lengthChange": true,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+        });
+    });
+    
+ 
+    function fetch_data(sc,ow,produk,warna,sales_group){
+        //datatables
+        $('#example1').DataTable({ 
+            "dom": "<'row'<'col-sm-4'l><'col-sm-5'i><'col-sm-3'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-5'><'col-sm-7'p>>",
+            "processing": true, 
+            "serverSide": true, 
+            "order": [4, "asc"], 
+            "aLengthMenu": [[50, 100, 500, 1000, -1], [50, 100, 500, 1000, "All"]],
+            "iDisplayLength": 50,
+
+            "paging": true,
+            "lengthChange": true,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+             
+            "ajax": {
+                "url": "<?php echo site_url('report/outstandingow/get_data')?>",
+                "type": "POST",
+                "data": function ( data ) {
+                    data.sc     = sc;
+                    data.ow     = ow;
+                    data.produk = produk;
+                    data.warna  = warna;
+                    data.sales_group  = sales_group;
+                },beforeSend: function () {
+                  //please_wait(function(){});
+                  $('#btn-filter').button('loading');
+                }, complete: function () {
+                  //unblockUI( function(){});
+                  $('#btn-filter').button('reset');
+                },
+            },
+ 
+            "columnDefs": [
+                { 
+                  "targets": [0], 
+                  "orderable": false, 
+                },
+                { 
+                  "targets": [ 6 ], 
+                  "width" : 120,
+                },
+                { 
+                  "targets": [ 8 ], 
+                  "className": 'text-right', 
+                  "width" : 80,
+                },
+            ],
+           
+        });
+
+    }
+
+    $('#btn-filter').click(function(){ //button filter event click
+      btn_filter();
+    });
+    
+    function btn_filter(){
+        $('#btn-filter').button('loading');
+        $('#example1').DataTable().destroy();
+        
+        var sc          = $('#sc').val();
+        var ow          = $('#ow').val();
+        var produk      = $('#produk').val();
+        var warna       = $('#warna').val();
+        var sales_group = $('#sales_group').val();
+       
+        fetch_data(sc,ow,produk,warna,sales_group);
+          //table.ajax.reload( function(){
+        $('#btn-filter').button('reset');
+          //});  //just reload table
+    }
+
+    function event_input(event){ 
+      if(event.keyCode == 13) {
+          event.preventDefault();
+          btn_filter();
+      }
+    }
+     
+
+</script>
+
+</body>
+</html>
