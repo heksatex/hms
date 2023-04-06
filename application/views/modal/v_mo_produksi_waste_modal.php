b4c9565e298f8ad18d9ec6ba9339f65ea7ef7567
galihguspa <galihdrm@gmail.com>
2023-04-06 09:08:29 +0700

    fix alert

--- application/views/modal/v_mo_produksi_waste_modal.php
+++ application/views/modal/v_mo_produksi_waste_modal.php
@@ -1638,7 +1638,7 @@
 					if(waste_rm_lot_valid == false){
 						alert_bootbox("Produk atau Lot  Konsumsi Bahan Baku tidak ada  ! ")
 					}
-					alert(rm_rows)
+					// alert(rm_rows)
 				}
 
 			}
