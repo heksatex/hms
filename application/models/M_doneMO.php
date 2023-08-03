8c753e42b872e861e338cd38e04d8f4731a75254
galihguspa <galihdrm@gmail.com>
2023-08-03 09:44:34 +0700

    fix report done MO

--- application/models/M_doneMO.php
+++ application/models/M_doneMO.php
@@ -19,7 +19,7 @@ class M_doneMO extends CI_Model
         return $this->db->query("SELECT d.nama as departemen, mrp.kode, mrp.tanggal, mrp.dept_id, mrp.con_mtr, mrp.con_kg, mrp.prod_mtr, mrp.prod_kg, mrp.waste_mtr, mrp.waste_kg, mrp.adj_mtr, mrp.adj_kg, mrp.status
                                 FROM mrp_production_done mrp 
                                 INNER JOIN departemen d ON mrp.dept_id = d.kode 
-                                WHERE mrp.tanggal >= '$tgl_dari' AND mrp.tanggal <= '$tgl_sampai' ")->result();
+                                WHERE mrp.dept_id = '$dept' AND mrp.tanggal >= '$tgl_dari' AND mrp.tanggal <= '$tgl_sampai' ")->result();
     }
 
 }
