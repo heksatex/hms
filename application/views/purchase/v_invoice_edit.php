cfdb929580fe1a706411b5123429e202ca7da6dc
RONI <RONI@DESKTOP-8O6CAN2>
2025-08-13 11:52:37 +0700

    update git ignore

--- application/views/purchase/v_invoice_edit.php
+++ application/views/purchase/v_invoice_edit.php
@@ -111,61 +111,34 @@
                                                     <label class="form-label">Supplier</label>
                                                 </div>
                                                 <div class="col-xs-8 col-md-8 text-uppercase">
-                                                    <span><?= $inv->supplier ?></span>
+                                                    <input class="form-control" value="<?= $inv->supplier ?>" disabled>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <!--                                        <div class="form-group">
-                                                                                    <div class="col-md-12 col-xs-12">
-                                                                                        <div class="col-xs-4">
-                                                                                            <label class="form-label">No PO</label>
-                                                                                        </div>
-                                                                                        <div class="col-xs-8 col-md-8 text-uppercase">
-                                                                                            <span><?= $inv->no_po ?></span>
-                                                                                        </div>
-                                                                                    </div>
-                                                                                </div>-->
-                                        <div class="form-group">
                                             <div class="col-md-12 col-xs-12">
                                                 <div class="col-xs-4">
-                                                    <label class="form-label">Order Date</label>
+                                                    <label class="form-label">Origin</label>
                                                 </div>
                                                 <div class="col-xs-8 col-md-8 text-uppercase">
-                                                    <span><?= date("l, d M Y H:i:s", strtotime($inv->order_date)) ?></span>
+                                                  <input class="form-control" value="<?= $inv->origin ?>" disabled>
+                                                    <input type="hidden" class="form-control pull-right input-sm" name="origin" value="<?= $inv->origin ?>" readonly> 
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group">
                                             <div class="col-md-12 col-xs-12">
                                                 <div class="col-xs-4">
-                                                    <label class="form-label">Mata Uang</label>
+                                                    <label class="form-label">Tgl dibuat</label>
                                                 </div>
                                                 <div class="col-xs-8 col-md-8 text-uppercase">
-                                                    <span><?= $inv->mata_uang ?></span>
+                                                    <input class="form-control" value="<?= date("Y-m-d H:i:s", strtotime($inv->created_at)) ?>" disabled>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="col-md-12 col-xs-12">
-                                                <div class="col-xs-4">
-                                                    <label class="form-label required">Kurs</label>
-                                                </div>
-                                                <div class="col-xs-8 col-md-8 text-uppercase">
-                                                    <input type="text" class="form-control pull-right input-sm" name="nilai_matauang" value="<?= number_format($inv->nilai_matauang, 0, '', '') ?>" <?= ($inv->status === 'draft') ? '' : "readonly" ?>>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
                                             <div class="col-md-12 col-xs-12">
                                                 <div class="col-xs-4">
                                                     <label class="form-label">No PO</label>
                                                 </div>
                                                 <div class="col-xs-8 col-md-8 text-uppercase">
-                                                    <span><?= $inv->no_po ?></span>
+                                                    <input class="form-control" value="<?= $inv->no_po ?>" disabled>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group">
                                             <div class="col-md-12 col-xs-12">
                                                 <div class="col-xs-4">
                                                     <label class="form-label required">Periode ACC</label>
@@ -180,7 +153,7 @@
                                                         ?>
                                                         <select class="form-control select2" name="periode" id="periode">
                                                             <?php
-                                                            $periodeNow = $inv->periode ?? date("Y/m");
+                                                            $periodeNow = $inv->periode ?? date("Y/m", strtotime($inv->created_at));
                                                             $selected = "selected";
 
                                                             foreach ($periode as $kk => $val) {
@@ -197,6 +170,7 @@
 
                                                 </div>
                                             </div>
+                                            
                                         </div>
                                     </div>
                                     <div class="col-md-6 col-xs-12">
@@ -209,8 +183,6 @@
                                                     <input type="text" class="form-control pull-right input-sm" name="no_invoice_supp" value="<?= $inv->no_invoice_supp ?>" <?= ($inv->status === 'draft') ? '' : "readonly" ?>> 
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group">
                                             <div class="col-md-12 col-xs-12">
                                                 <div class="col-xs-4">
                                                     <label class="form-label">Tgl Invoice Supplier</label>
@@ -220,8 +192,6 @@
                                                 </div>
                                                 <button type="submit" id="form-inv-submit" style="display: none"></button>
                                             </div>
-                                        </div>
-                                        <div class="form-group">
                                             <div class="col-md-12 col-xs-12">
                                                 <div class="col-xs-4">
                                                     <label class="form-label">No SJ Supplier</label>
@@ -230,8 +200,6 @@
                                                     <input type="text" class="form-control pull-right input-sm" name="no_sj_supp" value="<?= $inv->no_sj_supp ?>" <?= ($inv->status === 'draft') ? '' : "readonly" ?>> 
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group">
                                             <div class="col-md-12 col-xs-12">
                                                 <div class="col-xs-4">
                                                     <label class="form-label">Tgl SJ</label>
@@ -240,14 +208,20 @@
                                                     <input type="datetime-local" class="form-control pull-right input-sm" name="tanggal_sj" value="<?= $inv->tanggal_sj ?>" <?= ($inv->status === 'draft') ? '' : "readonly" ?>> 
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group">
                                             <div class="col-md-12 col-xs-12">
                                                 <div class="col-xs-4">
-                                                    <label class="form-label">Origin</label>
+                                                    <label class="form-label required">Kurs</label>
                                                 </div>
                                                 <div class="col-xs-8 col-md-8 text-uppercase">
-                                                    <input type="text" class="form-control pull-right input-sm" name="origin" value="<?= $inv->origin ?>" readonly> 
+                                                    <input type="text" class="form-control pull-right input-sm" name="nilai_matauang" value="<?= number_format($inv->nilai_matauang, 0, '', '') ?>" <?= ($inv->status === 'draft') ? '' : "readonly" ?>>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12 col-xs-12">
+                                                <div class="col-xs-4">
+                                                    <label class="form-label">Mata Uang</label>
+                                                </div>
+                                                <div class="col-xs-8 col-md-8 text-uppercase">
+                                                    <span><?= $inv->mata_uang ?></span>
                                                 </div>
                                             </div>
                                         </div>
