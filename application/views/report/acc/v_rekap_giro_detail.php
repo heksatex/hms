470e8caaec8445cd31ff22b4fe00046ad922df75
RONI <RONI@DESKTOP-8O6CAN2>
2026-03-10 09:06:55 +0700

    update aktif non aktif kode coa

--- application/views/report/acc/v_rekap_giro_detail.php
+++ application/views/report/acc/v_rekap_giro_detail.php
@@ -13,6 +13,7 @@ foreach ($data as $key => $value) {
         <td><?= ($value->partner_nama === "") ? $value->lain2 : $value->partner_nama ?></td>
         <td><?= $value->transinfo ?></td> 
         <td><?= $value->kode_coa ?></td>
+        <td><?= $value->coa ?></td>
         <td class="text-right"><?= number_format($value->nominal, 2) ?></td>
     </tr>
 
@@ -20,7 +21,7 @@ foreach ($data as $key => $value) {
 if($total > 0) {
     ?>
     <tr>
-        <td colspan="8"></td>
+        <td colspan="9"></td>
         <td> <strong>Total</strong></td>
         <td class="text-right"><?= number_format($total, 2) ?></td>
     </tr>
