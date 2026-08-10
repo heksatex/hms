1821cde4e5bbc748ae4f58f09e1097b11c800aae
RONI <RONI@DESKTOP-8O6CAN2>
2026-08-10 12:59:37 +0700

    update

--- application/libraries/excelexp/Memorialpembelian.php
+++ application/libraries/excelexp/Memorialpembelian.php
@@ -99,7 +99,7 @@ class Memorialpembelian {
                 $sheet->setCellValue("E{$row}", $grandTotalKredit);
             }
             $sheet->getStyle("D2:D{$row}")->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
-            $sheet->getStyle("E2:D{$row}")->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
+            $sheet->getStyle("E2:E{$row}")->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
 
             $nm = str_replace("/", "_", $data["periode"]);
             $filename = "jurnal {$data['jurnal']} {$nm} {$data["filter"]}";
